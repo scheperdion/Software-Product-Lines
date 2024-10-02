e656795154ac19c521cb3757bb3b7c9094e5bbb4
neo <dion.scheper@ru.nl>
2024-10-02 15:32:01 +0200

    added chatclient chatserver

---
+++ Featurehouse/HelloWorld-FH-Java/features/Chatter/CommandLineInterface.java
@@ -0,0 +1,32 @@
+//import messages.Message;
+//import messages.MessageColorPlugin;
+//import ui.MessageObserver;
+//import ui.UserInterface;
+//
+//import java.util.Scanner;
+//
+//public class CommandLineInterface implements MessageObserver, UserInterface {
+//    private final Client c;
+//    public CommandLineInterface(Client c) {
+//        this.c = c;
+//        c.addObserver(this);
+//    }
+//    @Override
+//    public void notify(Message message) {
+//        System.out.println(MessageColorPlugin.getInstance().getWithColor(message.getString()));
+//    }
+//
+//    @Override
+//    public void mainLoop() {
+//        final Scanner scanner = new Scanner(System.in);
+//        while(true) {
+//            System.out.printf("> \n");
+//            this.sendMessage(scanner.nextLine());
+//        }
+//    }
+//
+//    @Override
+//    public void sendMessage(String message) {
+//        c.send(new Message(message, null));
+//    }
+//}
