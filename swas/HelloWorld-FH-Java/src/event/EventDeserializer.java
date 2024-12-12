f5b2da89d9f3a72493c0974d9bee2ce133bf8327
TerenceBeijloos <42936949+TerenceBeijloos@users.noreply.github.com>
2024-12-12 15:47:34 +0100

    working map in extended ui

--- swas/HelloWorld-FH-Java/src/event/EventDeserializer.java
+++ swas/HelloWorld-FH-Java/src/event/EventDeserializer.java
@@ -2,17 +2,24 @@ package event;
 
 import com.google.gson.Gson; 
 import com.google.gson.GsonBuilder; 
-import com.google.gson.typeadapters.RuntimeTypeAdapterFactory;  
+import com.google.gson.typeadapters.RuntimeTypeAdapterFactory; 
 
-public    class    EventDeserializer {
+/**
+ * TODO description
+ */
+public   class  EventDeserializer {
 	
 	private RuntimeTypeAdapterFactory<AbstractEvent> runtimeTypeAdapterFactory;
 
 	
 
-	public EventDeserializer() {
+	public EventDeserializer  () {
 		this.runtimeTypeAdapterFactory = RuntimeTypeAdapterFactory.of(AbstractEvent.class, "type");
 		this.runtimeTypeAdapterFactory.registerSubtype(NoEvent.class, "noevent");
+	
+		this.runtimeTypeAdapterFactory.registerSubtype(TsunamiEvent.class, "tsunami");
+	
+		this.runtimeTypeAdapterFactory.registerSubtype(ThunderstormEvent.class, "thunderstorm");
 	}
 
 	
